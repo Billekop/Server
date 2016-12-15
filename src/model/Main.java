ddd0d4292527f8db4d4e0827519d7c44ca0ec27a
Alexander <alexanderskov@live.dk>
2016-12-15 20:50:00 +0100

    init commit

---
+++ src/model/Main.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Created by Ejer on 29-11-2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("en tester");
+
+        //opret bruger
+        //slet bog
+        //vis alle brugere
+    }
+}
